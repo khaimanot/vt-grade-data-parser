a77f6ef445dc5ce5efaf33d34c1735a5e803d75e
khaimanot <khaimanot@vt.edu>
2023-01-21 19:01:27 -0500

    allow for course name instead of subject + number

--- grades.py
+++ grades.py
@@ -2,44 +2,59 @@ import csv
 from sys import argv
 from os import makedirs
 
-def main():
-    # check for correct number of arguments
-    if len(argv) != 3:
-        print("Give the department and the course number as two arguments (ex. \"python grades.py CS 1114\")")
-        exit()
-    
-    # parse data
+def parse_data(mode: str):
     with open("./Grade Distribution.csv", 'r') as input:
         reader = csv.reader(input)
         firstline = True
         gpasum = 0.0
         gpacount = 0
         name = ""
+        name2 = ""
         instructors = {}
-        for row in reader:
-            if firstline: # skip field names
-                firstline = False
-                continue
-            if row[2] == argv[1] and row[3] == argv[2]:
-                # update overall gpa and course name
-                gpasum += float(row[6])
-                gpacount += 1
-                if name != row[4]:
-                    name = row[4]
-                # update instructor gpa
-                instname = row[5]
-                if instname not in instructors:
-                    instructors[instname] = []
-                instructors[instname].append(float(row[6]))
+        if mode == "coursecode":
+            name2 = f"{argv[1]}{argv[2]}"
+            for row in reader:
+                if firstline: # skip field names
+                    firstline = False
+                    continue
+                if row[2] == argv[1] and row[3] == argv[2]:
+                    # update overall gpa and course name
+                    gpasum += float(row[6])
+                    gpacount += 1
+                    if name != row[4]:
+                        name = row[4]
+                    # update instructor gpa
+                    instname = row[5]
+                    if instname not in instructors:
+                        instructors[instname] = []
+                    instructors[instname].append(float(row[6]))
+        elif mode == "coursename":
+            for row in reader:
+                if firstline: # skip field names
+                    firstline = False
+                    continue
+                if row[4] == argv[1]:
+                    # update overall gpa and course name
+                    gpasum += float(row[6])
+                    gpacount += 1
+                    if name != row[4]:
+                        name = row[4]
+                    if len(name2) == 0:
+                        name2 = f"{row[2]}{row[3]}"
+                    # update instructor gpa
+                    instname = row[5]
+                    if instname not in instructors:
+                        instructors[instname] = []
+                    instructors[instname].append(float(row[6]))
         # check that data was found
         if gpacount == 0:
             print("Course not found")
             exit()
         # write out the parsed data
         makedirs("results", exist_ok=True)
-        filename = f"results/{argv[1]}{argv[2]}.txt"
+        filename = f"results/{name2}.txt"
         with open(filename, 'w') as output:
-            header = argv[1] + argv[2] + " " + name
+            header = name2 + " " + name
             output.write(header + '\n')
             line = ""
             for i in range(len(header)):
@@ -52,7 +67,19 @@ def main():
                 instgpasum = sum(sorted_instructors[instname])
                 instgpacount = len(sorted_instructors[instname])
                 output.write(instname + ": " + str(round(instgpasum / instgpacount, 2)) + '\n')
-            print(f"Average GPA is {str(avg)} with data from {len(sorted_instructors)} instructors.\nMore info in {filename}.")
+            print(f"Average GPA for {header} is {str(avg)} with data from {len(sorted_instructors)} instructors.\nMore info in {filename}.")
+
+def main():
+    # check for correct number of arguments
+    if len(argv) == 3: # subject + course number
+        parse_data("coursecode")
+    elif len(argv) == 2: # course name
+        parse_data("coursename")
+    else:
+        print("Give the department and the course number as two arguments (ex. python3 grades.py CS 3214)")
+        print("or give the name of the course as a single argument in quotes (ex. python3 grades.py \"Computer Systems\"")
+        exit()
+    
 
 
 if __name__ == "__main__":
