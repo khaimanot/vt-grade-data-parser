dde58159e4210b8bd5ae1258eedc9b86417c872a
khaimanot <khaimanot@vt.edu>
2023-01-27 03:27:27 -0500

    reworked code to use pandas library

--- grades.py
+++ grades.py
@@ -1,125 +1,44 @@
-import csv
+import pandas
 from sys import argv
 from os import makedirs
 
 
-def search_by_coursecode(arg1: str, arg2: str, terms: str):
-    values = {}
 
-    gpasum = 0.0
-    gpacount = 0
-    name = ""
-    name2 = f"{arg1}{arg2}"
-    instructors = {}
-
-    with open("./Grade Distribution.csv", 'r') as input:
-        reader = csv.reader(input)
-        firstline = True
-        for row in reader:
-            if firstline: # skip csv field names
-                firstline = False
-                continue
-            if row[2] == arg1 and row[3] == arg2 and row[1] in terms: # check subject, number, and term
-                # update overall gpa and course name
-                gpasum += float(row[6])
-                gpacount += 1
-                if name != row[4]:
-                    name = row[4]
-                # update instructor gpa
-                instname = row[5]
-                if instname not in instructors:
-                    instructors[instname] = []
-                instructors[instname].append(float(row[6]))
-
-    values['gpasum'] = gpasum
-    values['gpacount'] = gpacount
-    values['name'] = name
-    values['name2'] = name2
-    values['instructors'] = instructors
-    return values
-
-
-def search_by_coursename(arg: str, terms: str):
-    values = {}
-
-    gpasum = 0.0
-    gpacount = 0
-    name = ""
-    name2 = ""
-    instructors = {}
-
-    with open("./Grade Distribution.csv", 'r') as input:
-        reader = csv.reader(input)
-        firstline = True
-        for row in reader:
-            if firstline: # skip field names
-                firstline = False
-                continue
-            if row[4] == arg and row[1] in terms: # check coursename and term
-                # update overall gpa and course name
-                gpasum += float(row[6])
-                gpacount += 1
-                if name != row[4]:
-                    name = row[4]
-                if len(name2) == 0:
-                    name2 = f"{row[2]}{row[3]}"
-                # update instructor gpa
-                instname = row[5]
-                if instname not in instructors:
-                    instructors[instname] = []
-                instructors[instname].append(float(row[6]))
-
-    values['gpasum'] = gpasum
-    values['gpacount'] = gpacount
-    values['name'] = name
-    values['name2'] = name2
-    values['instructors'] = instructors
-    return values
-
-def all_terms(terms: str):
-    return terms == "Fall Winter Spring Summer I Summer II"
-
-def print_results(results: dict, terms: str):
-    gpasum = results['gpasum']
-    gpacount = results['gpacount']
-    name = results['name']
-    name2 = results['name2']
-    instructors = results['instructors']
-    # write out the parsed data
+def write_results(course: str, terms: str, header: str, filename: str, avg: float, totals: list):
     makedirs("results", exist_ok=True)
-    filename = f"results/{name2}.txt"
     with open(filename, 'w') as output:
-        header = name2 + " " + name
-        output.write(f"{header}\n{terms}\n")
-        line = ""
-        for i in range(len(header)):
-            line += '-'
-        avg = round((gpasum / gpacount), 2)
-        output.write(f"{line}\nAverage GPA: {str(avg)}\n\n")
-        sorted_instructors = {}
-        for instname in list(instructors.keys()):
-            instgpasum = sum(instructors[instname])
-            instgpacount = len(instructors[instname])
-            sorted_instructors[instname] = round(instgpasum / instgpacount, 2)
-        sorted_instructors = dict(sorted(sorted_instructors.items(), key=lambda x:x[1], reverse=True))
-        for instname in sorted_instructors:
-            output.write(instname + ": " + str(sorted_instructors[instname]) + '\n')
-        print(f"Average GPA for {header} is {str(avg)} with data from {len(sorted_instructors)} instructors in these terms: {terms}.")
-        print(f"More info in {filename}.")
+        output.write(header)
+        output.write(f"Average GPA: {avg}\n\n")
+        for total in totals:
+            output.write(f"{total[0]}: {total[1]}\n")
+    print(f"Average GPA is {str(avg)} for \"{course}\" with data from {len(totals)} instructors in these terms: {terms}.")
+    print(f"More info in {filename}.\n")
 
 
-def parse_data(arg1: str, arg2: str, terms: str):
-    if arg2:
-        results = search_by_coursecode(arg1, arg2, terms)
+def parse(data: pandas.DataFrame, arg: str, term_list: list):
+    if data.empty:
+        print(f"No data found for {arg}.\n")
     else:
-        results = search_by_coursename(arg1, terms)
+        coursename = data.iat[0, 4]
+        coursecode = f"{data.iat[0, 2]}{data.iat[0, 3]}"
+        header = f"{coursecode} {coursename}\n"
+        course = f"{coursecode} {coursename}"
+        namelen = len(header)
 
-    # check that data was found
-    if results['gpasum'] == 0:
-        print(f"No data found for {arg1}{arg2}.")
-    else:
-        print_results(results, terms)
-    print() # place blank line between requested courses' results
+        terms = ""
+        for term in term_list:
+            terms += f"{term}, "
+        terms = terms[:-2]
+        header += f"{terms}\n"
+        for i in range(namelen):
+            header += '-'
+        header += '\n'
+        filename = f"results/{coursecode}.txt"
+        instructors = data.Instructor.unique()
+        insttotals = [(inst, round(data[data["Instructor"] == inst]['GPA'].sum() / len(data[data["Instructor"] == inst].index), 2)) for inst in instructors]
+        insttotals.sort(key=lambda x: x[1], reverse=True)
+        avg_gpa = round(data.GPA.mean(), 2)
+        write_results(course, terms, header, filename, avg_gpa, insttotals)
 
 
 def process_arguments():
@@ -130,27 +49,36 @@ def process_arguments():
         print("You can give multiple courses at a time.\n")
         print("You can give any of these optional arguments before listing the courses:")
         print("-h or --help:    Display this help information")
-        print("-t or --terms:   Give a list of any semester terms to limit the search (ex. -t \"Fall Spring Winter Summer I Summer II\").")
-        print("                 The quotes must be used, and you can give any number of the possible terms.")
+        print("-t or --terms:   Give a list of any semester terms to limit the search (ex. python3 grades.py -t \"Fall,Spring,Winter,Summer I,Summer II\"...).")
+        print("                 The quotes must be used, and a single comma must separate each term. You can give any number of the possible terms.")
         raise SystemExit()
     i = 1
-    terms = "Fall Winter Spring Summer I Summer II"
+    t = "Fall,Winter,Spring,Summer I,Summer II"
     while argv[i].startswith("-"):
         if argv[i] == "--terms" or argv[i] == "-t":
             i += 1
-            terms = argv[i]
+            t = argv[i]
         i += 1
-    
+    terms = t.split(',')
+
+    data = pandas.read_csv('Grade Distribution.csv')
+    # rename columns so they can be accessed as fields of data in while loop
+    data.rename(columns={'Course Title': 'CourseTitle', 'Course No.': 'CourseNo', 'Academic Year': 'AcademicYear'}, inplace=True)
     while i < len(argv):
         if i == len(argv) - 1: # final argument is single arg coursename
-            parse_data(argv[i], "", terms)
+            data2 = data[data.CourseTitle == argv[i]]
+            parse(data2[data2['Term'].isin(terms)], argv[i], terms)
             break
         if argv[i + 1].isnumeric(): # current argument is part of subj + num
-            parse_data(argv[i], argv[i + 1], terms)
+            data2 = data[data.Subject == argv[i]]
+            data3 = data2[data2.CourseNo == int(argv[i + 1])]
+            parse(data3[data3['Term'].isin(terms)], argv[i] + argv[i + 1], terms)
             i += 2
         else: # current argument is single arg coursename
-            parse_data(argv[i], "", terms)
+            data2 = data[data.CourseTitle == argv[i]]
+            parse(data2[data2['Term'].isin(terms)], argv[i], terms)
             i += 1
+
 
 def main():
     if len(argv) == 1: # no arguments have been given
